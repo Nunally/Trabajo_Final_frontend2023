121fcef42bdc7ad68444acfee37e3047a22bd7a4
Nunally <74014577+Nunally@users.noreply.github.com>
2023-01-05 14:44:16 -0300

    Add files via upload

---
+++ Trabajo_Final/Trabajo_Final.js
@@ -0,0 +1,17 @@
+let botonCambiarFoto = document.getElementById (BotonCambiarFoto)
+let cambioImagen = document.getElementById (cambiarFoto)
+let usuarioNuevo 
+
+
+
+botonCambiarFoto.onclick = function (){
+   if 
+    cambioImagen.src =  
+  }
+
+  /**
+   * quisiera que el usuario pueda cambiar la foto de perfil
+   * poder agregar mas casillas en donde se encuentra la experiencia laboral y educativa
+   * hacer mas grande la casilla de comentarios
+   * darle un toque mas lindo a la pagina
+   */
